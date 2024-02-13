545e26f649f2b2ca31d8c0bef20c3aaed8b252d8
Young <gooker_young@163.com>
2024-02-13 15:11:40 +0800

    update config

--- ncmpy/main.py
+++ ncmpy/main.py
@@ -1,11 +1,15 @@
 import sys
 
 from PySide2.QtWidgets import QApplication
+from qt_material import apply_stylesheet
 
 from gui.mainwindow import MainWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    apply_stylesheet(app, theme='light_amber.xml')
+
     win = MainWindow()
     win.show()
+
     app.exec_()
