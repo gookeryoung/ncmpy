d06496422a9e5f85abed0c9f81a948eb1ba81ef4
Young <gooker_young@163.com>
2024-02-13 16:00:26 +0800

    ~

--- ncmpy/gui/mainwindow.py
+++ ncmpy/gui/mainwindow.py
@@ -1,4 +1,5 @@
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtCore import QDir, QStringListModel
+from PySide2.QtWidgets import QMainWindow, QFileDialog
 
 from gui.ui_mainwindow import Ui_MainWindow
 
@@ -8,3 +9,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__(parent)
 
         self.setupUi(self)
+
+        self.pbOpenOutDir.clicked.connect(self.open_out_directory)
+
+        self.out_dirs = QStringListModel([])
+
+    def open_out_directory(self):
+        out_dir = QFileDialog.getExistingDirectory(
+            self, '输出目录', QDir.homePath(), QFileDialog.ShowDirsOnly | QFileDialog.DontResolveSymlinks
+        )
+        if len(out_dir):
+            self.cbOutDir.setCurrentText(out_dir)
