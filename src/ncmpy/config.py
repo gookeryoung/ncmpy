6c08bb7ae7529f8637978ae03e673f9f2b61f9cf
Young <gooker_young@163.com>
2024-02-21 22:14:47 +0800

    ~

--- src/ncmpy/config.py
+++ src/ncmpy/config.py
@@ -3,3 +3,25 @@ from pathlib import Path
 DIR_BASE = Path(__file__).parent.parent.parent
 DIR_TEST = DIR_BASE / "tests"
 DIR_TEST_FILE = DIR_TEST / "Now冥想 - 改善睡眠第2天 · 触摸式放松练习.ncm"
+
+WIN_STYLES = [
+    "dark_amber.xml",
+    "dark_blue.xml",
+    "dark_cyan.xml",
+    "dark_lightgreen.xml",
+    "dark_pink.xml",
+    "dark_purple.xml",
+    "dark_red.xml",
+    "dark_teal.xml",
+    "dark_yellow.xml",
+    "light_amber.xml",
+    "light_blue.xml",
+    "light_cyan.xml",
+    "light_cyan_500.xml",
+    "light_lightgreen.xml",
+    "light_pink.xml",
+    "light_purple.xml",
+    "light_red.xml",
+    "light_teal.xml",
+    "light_yellow.xml",
+]
--- src/ncmpy/main.py
+++ src/ncmpy/main.py
@@ -1,12 +1,13 @@
 import sys
 
 from PySide2.QtWidgets import QApplication
-from gui.mainwindow import MainWindow
 from qt_material import apply_stylesheet
+
+from gui.mainwindow import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    apply_stylesheet(app, theme="light_cyan_500.xml")
+    apply_stylesheet(app, theme="light_lightgreen.xml")
 
     win = MainWindow()
     win.show()
